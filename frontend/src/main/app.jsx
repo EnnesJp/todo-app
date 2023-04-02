39d00cc58d09fa94b4e3b12645fa25c349e4d9ab
João Pedro Ennes <65932730+EnnesJp@users.noreply.github.com>
2023-04-02 20:02:08 -0300

    Changing to react script

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,10 +1,15 @@
-import 'module/bootstrap/dist/css/bootstrap.min.css'
-import 'module/font-awesome/css/font-awesome.min.css'
-
 import React from 'react'
+// import Menu from '../template/Menu'
+// import Routes from './Routes'
+// import '../template/custom.css'
 
-export default props => (
-  <div className='container'>
-    <h1>App</h1>
-  </div>
+require('bootstrap/dist/css/bootstrap.min.css')
+require('font-awesome/css/font-awesome.min.css')
+
+
+const App = props => (
+    <div className="container">
+        <h1>Todo App</h1>
+    </div>
 )
+export default App
